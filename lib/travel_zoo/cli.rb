215c23855cd7e454a83fcbe818a38fa437af4434
ShiLi007 <lishidaniel@gmail.com>
2016-03-13 01:42:27 -0500

    added comments

--- lib/travel_zoo/cli.rb
+++ lib/travel_zoo/cli.rb
@@ -1,32 +1,39 @@
 class TravelZoo::CLI
 
+# the call method will be executed through TravelZoo::CLI.new.call in bin file. 
+#call method will call the other three methods that are fundations of the interface. 
+
   def call
     list_deals
     menu
     goodbye
   end
 
+# the list_deals method will put the travel deals to console for the users to view.
+
   def list_deals
     puts "ATTENTION!!!Today's top 20 deals" 
     puts "-------------------------------------------------------"
-    TravelZoo::Deal.scraper.each_with_index do |key, i|
+    TravelZoo::Deal.scraper.each_with_index do |key, i| #call on ravelZoo::Deal.scraper and print and numerate each deal
     puts "#{i+1}. #{key.name}"
   end
   end
 
+# the menu method will let the users choose the deal that they want to see the details. 
+
   def menu 
-    input = nil
-    while input !="exit"
+    input = nil 
+    while input !="exit"  #unless the user input "exit" or "lsit" the loop will take a number from user and print out the detail of the corresponding deal
       puts "-------------------------------------------------------"
       puts "Select the deal you like to for more info!(Enter the number 1-20.Some deals may not have details).Enter a different number for different deal details"  
       puts "Enter 'list' back to deal list or 'exit' to exit the program"
-      input = gets.strip.downcase
+      input = gets.strip.downcase #the input will be saved here
       puts "-------------------------------------------------------"
-      if input.to_i > 0 && input.to_i <21
-        deal = TravelZoo::Deal.select_deal(input.to_i)
-        display_details(deal)
+      if input.to_i > 0 && input.to_i <21 # make sure the number is between 1-20 since we only have 20 deals
+        deal = TravelZoo::Deal.select_deal(input.to_i) #call on the select_deal method to select the deal since all the deals scraped are saved in @@deals
+        display_details(deal) #after select the deall user wants to see more, call on display_details to show the details of the deal
       elsif input === "list" 
-        list_deals   
+        list_deals   #call the list_deals method again if user wants to see the deal list again 
       else
         puts "Invalid input, please follow below instruction!"
       end
@@ -35,10 +42,10 @@ class TravelZoo::CLI
 
 
 def display_details(deal)
-  puts "#{deal.details}"
+  puts "#{deal.details}"  #shows the details of the deal, this method is based on scrape_details method in deal.rb
 end
 
   def goodbye
-    puts "See you tomorrow for more deals!"
+    puts "See you tomorrow for more deals!" #method to show exit to the program 
   end
 end
--- lib/travel_zoo/deal.rb
+++ lib/travel_zoo/deal.rb
@@ -1,7 +1,7 @@
 class TravelZoo::Deal
-  attr_accessor:name,:details,:url
+  attr_accessor:name,:details,:url 
 
-  @@deals = []
+  @@deals = []  #the array will store all the deals scraped from the website 
 
   def initialize(name = "", url = "")
     @name = name
@@ -10,27 +10,27 @@ class TravelZoo::Deal
     scrape_details
   end
 
-  def self.scraper
-    doc = Nokogiri::HTML(open("http://www.travelzoo.com/top20/"))
-      doc.css("span.tzsg-h2 a").each do |deal|
-        deal_name = deal.text.strip
+  def self.scraper 
+    doc = Nokogiri::HTML(open("http://www.travelzoo.com/top20/")) # HTTP request by open--URI and scraping the deals using Nokogiri
+      doc.css("span.tzsg-h2 a").each do |deal| #CSS selectors to retrieve the intended piece of data
+        deal_name = deal.text.strip #store the data in variables to be used to initialize instances 
         deal_url = deal.attr("href")
-        deal = self.new(deal_name,deal_url)
-        @@deals << deal
+        deal = self.new(deal_name,deal_url) #create new instance of deal 
+        @@deals << deal #store each deal to the array 
     end
-    self.deals
+    self.deals #return @@deals
   end
 
-  def scrape_details
+  def scrape_details  #again,further scraped the detail of each deal through another scraping method
     doc = Nokogiri::HTML(open(@url))
-    @details = doc.css("div.dealText").text
+    @details = doc.css("div.dealText").text 
   end
 
-  def self.select_deal(input)
+  def self.select_deal(input) #show a deal by index(or input-1) in the deals array,this is how user choose a number to see the deal
     @@deals[input-1]
   end
 
-  def self.deals
+  def self.deals #shows the deals in the deals array 
     @@deals
   end
 
